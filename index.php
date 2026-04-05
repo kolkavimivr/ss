f4bda5880f85a4d6a7361f4cfe74ad0b422eccc9
kolkavimivr <kolkavimivr@gmail.com>
2026-04-05 16:08:56 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
-$phone = $_GET['phone'] ?? '';
+$phone = $_REQUEST['phone'] ?? '';
 
 if (!empty($phone)) {
     file_put_contents('reg.txt', $phone . PHP_EOL, FILE_APPEND);
+    // תשובה נקייה ללא תווים נסתרים
     echo "id_list_message=f-success&";
 } else {
     echo "id_list_message=f-error&";
