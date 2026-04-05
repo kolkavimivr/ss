6dfd7fe0df03bfe08df1372c36b212bdacd245e6
kolkavimivr <kolkavimivr@gmail.com>
2026-04-05 16:19:04 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-ob_start();
 $phone = $_REQUEST['phone'] ?? '';
 if (!empty($phone)) {
     file_put_contents('reg.txt', $phone . PHP_EOL, FILE_APPEND);
 }
-ob_end_clean();
+header('Content-Type: text/plain');
 echo "id_list_message=f-success&";
 exit;
+?>
